42b78bb6f02e69212d1830a0dac298c99b49bdd5
asali <asali@andrew.cmu.edu>
2023-04-24 22:03:47 +0000

    hi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='s23p',
+      version='0.0.1',
+      description='s23p',
+      maintainer='Ahmad Ali',
+      maintainer_email='asali@andrew.cmu.edu',
+      license='MIT',
+      packages=['s23p'],
+      scripts=[],
+      long_description='''Project for openalex''')
+
