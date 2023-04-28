9bd5170ec859eb88357407e0b1891db8baed5fdf
asali <asali@andrew.cmu.edu>
2023-04-28 23:46:38 +0000

    s

--- s23p/main.py
+++ s23p/main.py
@@ -1,19 +1,18 @@
 from s23p import Works
 import argparse
-
-
 def main():
-    parser = argparse.ArgumentParser(description='Get bibliography or RIS format from a URL')
-    parser.add_argument('url', type=str, help='URL of the article')
-    parser.add_argument('-b', '--bib', action='store_true', help='output in bibliography format')
-    parser.add_argument('-r', '--ris', action='store_true', help='output in RIS format')
+    def get_output(url, output_format):
+        w = Works(url)
+        if output_format == 'bib':
+            return w.bib()
+        elif output_format == 'ris':
+            return print(w.ris)
+        else:
+            raise ValueError('bib or ris')
+    parser = argparse.ArgumentParser(description='Get bib or ris for DOI')
+    parser.add_argument('url', type=str, help='DOI')
+    parser.add_argument('-f', '--format', type=str, choices=['bib', 'ris'], default='bib', help='format (default: bib)')
     args = parser.parse_args()
+    output = get_output(args.url, args.format)
 
-    works = Works(args.url)
-    if args.bib:
-        print(works.bib)
-    elif args.ris:
-        print(works.ris)
-    else:
-        print('Please specify -b or -r for bibliography or RIS format, respectively.')
 
