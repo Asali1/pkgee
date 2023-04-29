97c219b9ed787e88036a68c64eb3ac9f1f329e8e
asali <asali@andrew.cmu.edu>
2023-04-29 00:10:32 +0000

    k

--- s23p/main.py
+++ s23p/main.py
@@ -3,18 +3,20 @@ commandline utility
 """
 import argparse
 from s23p import Works
+
+
 def main():
-    '''
+    """
     main
-    '''
+    """
     def get_output(url, output_format):
         w_doi = Works(url)
         if output_format == "bib":
             return w_doi.bib()
         elif output_format == "ris":
-            print(w_doi.ris)
-        else:
-            raise ValueError("bib or ris")
+            return w_doi.ris
+        
+    
     parser = argparse.ArgumentParser(description="Get bib or ris for DOI")
     parser.add_argument("url", type=str, help="DOI")
     parser.add_argument(
@@ -26,4 +28,5 @@ def main():
         help="format (default: bib)",
     )
     args = parser.parse_args()
-
+    output = get_output(args.url, args.format)
+    return output
